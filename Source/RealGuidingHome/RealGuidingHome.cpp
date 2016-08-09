e3ed255d7830e4e53ebacdb5181b4bce2fd8f9ed
DESKTOP-0EM1B7Q\Roo <sam.katharina@gmail.com>
2016-08-10 01:27:16 +0200

    initial commit

---
+++ Source/RealGuidingHome/RealGuidingHome.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RealGuidingHome.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RealGuidingHome, "RealGuidingHome" );
---
+++ Source/RealGuidingHome/RealGuidingHomeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "RealGuidingHomeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALGUIDINGHOME_API ARealGuidingHomeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
